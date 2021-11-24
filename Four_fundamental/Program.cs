420fdbf4d36bbc5336650a3a6523bed4df2cc51b
kimsoosoo0928 <kimsoosoo0928@gmail.com>
2021-11-24 17:15:27 +0900

    first commit

---
+++ Four_fundamental/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Four_fundamental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 0;
+            int second = 0;
+
+            Console.WriteLine("숫자를 , 로 구분해서 두개 입력하세요");
+            string[] input = Console.ReadLine().Split(',');
+
+            first = int.Parse(input[0]);
+            second = int.Parse(input[1]);
+
+            int sum = first + second;
+            int sub = first - second;
+            int mul = first * second;
+            float div = (float)first / (float)second;
+
+            Console.WriteLine(sum);
+            Console.WriteLine(sub);
+            Console.WriteLine(mul);
+            Console.WriteLine(div);
+        }
+
+        
+    }
+}
---
+++ triangle_class/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace triangle_class
+{
+
+    class Triangle
+    {
+        int side_a;
+        int side_b;
+        int side_c;
+
+        int round;
+
+        public int Round
+        {
+            get
+            {
+                return round;
+            }
+            set
+            {
+                round = value;
+            }
+        }
+
+
+        public Triangle(int _side_a, int _side_b, int _side_c) // 클래스 안에 클래스 
+        {
+            side_a = _side_a;
+            side_b = _side_b;
+            side_c = _side_c;
+
+            Round = side_a + side_b + side_c;
+        }
+
+        public override string ToString()
+        {
+            return $"Draw({side_a}, {side_b}, {side_c})";
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("숫자를 ,로 나눠서 입력하세요");
+
+            
+            string[] input = Console.ReadLine().Split(",");
+
+            int side_a = int.Parse(input[0]);
+            int side_b = int.Parse(input[1]);
+            int side_c = int.Parse(input[2]);
+
+            Triangle trg = new(side_a, side_b, side_c);
+
+            Console.WriteLine(trg.Round);
+            Console.WriteLine(trg);
+            
+        }
+    }
+}
